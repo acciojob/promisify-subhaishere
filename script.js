3ef277deac1b590eff67ace6cc5837f386415a3c
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 11:45:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function promisify(callbackFunction) {
-    
+    return function(...args) {
+      return new Promise((resolve, reject) => {
+        const callback = function(error, value) {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(value);
+          }
+        }
+        args.push(callback);
+        callbackFunction.apply(this, args);
+      });
+    }
   }
 module.exports=promisify;  
